d273b23d9e34529a5b866701d39040f92cc28507
Sailor Priddy <35895538+sayofthelor@users.noreply.github.com>
2023-01-18 23:02:08 -0500

    Update Interp.hx

--- hscript/Interp.hx
+++ hscript/Interp.hx
@@ -111,6 +111,7 @@ class Interp {
 		#else
 		binops = new Hash();
 		#end
+		binops.set("is", function(e1,e2) return Std.isOfType(me.expr(e1), me.expr(e2)));
 		binops.set("+",function(e1,e2) return me.expr(e1) + me.expr(e2));
 		binops.set("-",function(e1,e2) return me.expr(e1) - me.expr(e2));
 		binops.set("*",function(e1,e2) return me.expr(e1) * me.expr(e2));
